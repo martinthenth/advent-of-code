a3606f52a8282802c0ad078b637c63a4b8a7fc85
Martin Nijboer <mrnijboer@gmail.com>
2024-12-08 04:43:48 +0100

    Add boilerplate day 3

---
+++ 3/main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPart1(t *testing.T) {
+	t.Run("example", func(t *testing.T) {
+		assert.Fail(t, "TODO")
+	})
+}
+
+func TestPart2(t *testing.T) {
+	t.Run("example", func(t *testing.T) {
+		assert.Fail(t, "TODO")
+	})
+}
